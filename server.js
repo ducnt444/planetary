657cd13c46fb4e624a8c4d613308b17f70c5ca18
ducnt <ducnt444@gmail.com>
2021-09-21 15:47:33 +0700

    update

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
-const express = require("express");
-const serveStatic = require("serve-static");
-const path = require("path");
+// const express = require("express");
+// const serveStatic = require("serve-static");
+// const path = require("path");
 
-const app = express();
+// const app = express();
 
-//here we are configuring dist to serve app files
-app.use("/", serveStatic(path.join(__dirname, "/dist")));
+// //here we are configuring dist to serve app files
+// app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
-// this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function(req, res) {
-  res.sendFile(path.join(__dirname, "/dist/index.html"));
-});
+// // this * route is to serve project on different page routes except root `/`
+// app.get(/.*/, function(req, res) {
+//   res.sendFile(path.join(__dirname, "/dist/index.html"));
+// });
 
-const port = process.env.PORT || 8080;
-app.listen(port);
-console.log(`app is listening on port: ${port}`);
+// const port = process.env.PORT || 8080;
+// app.listen(port);
+// console.log(`app is listening on port: ${port}`);
+
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
